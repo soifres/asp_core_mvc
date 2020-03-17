550424e0697af210b47f465877aba486445410a9
Sergey Underforest <soifres@gmail.com>
2020-03-17 17:52:04 +0300

    stp_1752

--- Startup.cs
+++ Startup.cs
@@ -40,10 +40,14 @@ namespace asp_core_mvc
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlite(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            
+            services.AddDefaultIdentity<IdentityUser>(options =>
+                options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            
             services.AddControllersWithViews();
-           services.AddRazorPages();
+           
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
